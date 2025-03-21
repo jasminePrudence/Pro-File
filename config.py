48227e3770efadf2e1983b912266b7cfd67ac161
Jasmine <yasmineprudence@yahoo.fr>
2025-03-21 14:04:16 +0100

    Supprimer les fichiers du dépôt tout en gardant les fichiers locaux

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")  # À définir sur Render
-    DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
